5a8a4024675d8a2b4d1f2c1cbf6725694abab0da
Sadwintch <raysin.maya1@gmail.com>
2021-11-23 17:45:15 +0200

    added generic scan func (not working)

--- AVLtree.cpp
+++ AVLtree.cpp
@@ -7,16 +7,17 @@ AVLTree::AVLTree(): root(nullptr){}
 
 
 template<class KeyElem, class Data>
-AVLTree::AVL_RESULT& AVLAdd(const &KeyElem key, const &Data data){
+AVLTree::AVL_RESULT& AVLInsert(const &KeyElem key, const &Data data){
 
     std::smart_ptr<TNode> new_node = new(TNode(key, data));
     bool node_exists = false;
+    
     if(this->root == nullptr){
-        root = *new_node;
+        root = new_node;
         return AVL_SUCCESS;    
     }
     else{
-        auto father = AVLSearchFather(&new_node, &node_exists);
+        auto father = AVLSearchFather(new_node, &node_exists);
         if (node_exists == true){
             father.data = new_node.data;
             return AVL_SUCCESS_W_CHANGE;
@@ -31,4 +32,23 @@ AVLTree::AVL_RESULT& AVLAdd(const &KeyElem key, const &Data data){
         }
     }
 
-} 
+}
+
+template<class KeyElem, class Data>
+void AVLScanInOrder(AVLTree& tree){
+
+    AVLScanInOrder_rec(tree.root);
+}
+
+//SUPER Generic inorder scan (can add other stuff to it)
+void AVLScanInOrder_rec(TNode* node){
+    if (node.height() == 0){
+        //do something with leaf
+        return;
+    }      //height(v) = max[h(Lv),h(Rv)] +1
+    
+    AVLScanInOrder_rec(node.left);
+    AVLScanInOrder_rec(node.right);
+
+    // do something();
+}
--- TNode.cpp
+++ TNode.cpp
@@ -1,11 +1,8 @@
 #include "AVLtree.h"
 #include "./course_files/library1.h"
 
-
-
-
 template<class KeyElem, class Data>
 TNode::TNode(const &KeyElem key, const &Data data):
     key(key), data(data),
-    right(nullptr), left(nullptr){
+    father(nullptr), left(nullptr), right(nullptr){
 }
